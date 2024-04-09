8c5539e45880991a7b43bf9e024efd8e4c36098c
mahz <marcohurtado358@gmail.com>
2024-04-08 19:58:31 -0500

    add method in user repository

--- backend/ms-security/src/main/java/microservises/mssegurity/Repositories/UserRepository.java
+++ backend/ms-security/src/main/java/microservises/mssegurity/Repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package microservises.mssegurity.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import microservises.mssegurity.Models.User;
 
 public interface UserRepository extends MongoRepository<User, String>{
-    
+     @Query("{'email': ?0}")
+    public Optional<User> getUserByEmail(String email);
 }

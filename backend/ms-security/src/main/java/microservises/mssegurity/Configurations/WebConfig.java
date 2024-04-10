5ca2c782ca5e2fc7885cd9f9c39fc8a5f7930749
eldari18 <juandario050201@gmail.com>
2024-04-10 10:38:36 -0500

    jwt secrets added

---
+++ backend/ms-security/src/main/java/microservises/mssegurity/Configurations/WebConfig.java
@@ -0,0 +1,24 @@
+package microservises.mssegurity.Configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import microservises.mssegurity.Interceptors.SecurityInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SecurityInterceptor securityInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(securityInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/public/**");
+
+    }
+}
---
+++ backend/ms-security/src/main/java/microservises/mssegurity/Services/ValidatorsService.java
@@ -0,0 +1,68 @@
+package microservises.mssegurity.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import microservises.mssegurity.Models.Permission;
+import microservises.mssegurity.Models.Role;
+import microservises.mssegurity.Models.RolePermission;
+import microservises.mssegurity.Models.User;
+import microservises.mssegurity.Repositories.PermissionRepository;
+import microservises.mssegurity.Repositories.RolePermissionRepository;
+import microservises.mssegurity.Repositories.UserRepository;
+
+@Service
+public class ValidatorsService {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private PermissionRepository thePermissionRepository;
+    @Autowired
+    private UserRepository theUserRepository;
+    @Autowired
+    private RolePermissionRepository theRolePermissionRepository;
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    public boolean validationRolePermission(HttpServletRequest request, String url, String method) {
+        boolean success = false;
+        User theUser = this.getUser(request);
+        if (theUser != null) {
+            Role theRole = theUser.getRole();
+            System.out.println("Antes URL " + url + " metodo " + method);
+            url = url.replaceAll("[0-9a-fA-F]{24}|\\d+", "?");
+            System.out.println("URL " + url + " metodo " + method);
+            Permission thePermission = this.thePermissionRepository.getPermission(url, method);
+            if (theRole != null && thePermission != null) {
+                System.out.println("Rol " + theRole.getName() + " Permission " + thePermission.getUrl());
+                RolePermission theRolePermission = this.theRolePermissionRepository.getRolePermission(theRole.get_id(),
+                        thePermission.get_id());
+                if (theRolePermission != null) {
+                    success = true;
+                }
+            } else {
+                success = false;
+            }
+        }
+        return success;
+    }
+
+    public User getUser(final HttpServletRequest request) {
+        User theUser = null;
+        String authorizationHeader = request.getHeader("Authorization");
+        System.out.println("Header " + authorizationHeader);
+        if (authorizationHeader != null && authorizationHeader.startsWith(BEARER_PREFIX)) {
+            String token = authorizationHeader.substring(BEARER_PREFIX.length());
+            System.out.println("Bearer Token: " + token);
+            User theUserFromToken = jwtService.getUserFromToken(token);
+            System.out.println("User from token: " + theUserFromToken);
+            if (theUserFromToken != null) {
+                theUser = this.theUserRepository.findById(theUserFromToken.get_id())
+                        .orElse(null);
+                theUser.setPassword("");
+            }
+        }
+        return theUser;
+    }
+}

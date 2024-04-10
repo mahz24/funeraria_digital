e8fcb54c8bfb58148210939bbde773b129df03c0
mahz <marcohurtado358@gmail.com>
2024-04-10 12:01:40 -0500

    acept pull incoming from @eldary

--- backend/ms-security/src/main/java/microservises/mssegurity/Controllers/UserController.java
+++ backend/ms-security/src/main/java/microservises/mssegurity/Controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -181,4 +182,9 @@ public class UserController {
                     .body(this.jsonResponsesService.getFinalJSON());
         }
     }
+
+    //@PatchMapping("/reset-password")
+    //public ResponseEntity<?> resetPassword(RequestBody User user){
+
+    //}
 }

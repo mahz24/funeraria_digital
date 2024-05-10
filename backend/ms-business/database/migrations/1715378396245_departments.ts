76da8987d95f807df22fb8095d6ac8f250d5b47a
juan-tangarife <juan.tangarife35246@ucaldas.edu.co>
2024-05-10 17:17:59 -0500

    first_migration

--- backend/ms-business/database/migrations/1715378396245_departments.ts
+++ backend/ms-business/database/migrations/1715378396245_departments.ts
@@ -1,21 +1,23 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'rooms'
+  protected tableName = 'departments'
 
-  public async up() {
+  public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('num')
+      table.increments('id')
       table.string('name')
-      table.string('description')
-      table.integer('capacity')
-      table.string('status')
+      table.string('location')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
 
-  public async down() {
+  public async down () {
     this.schema.dropTable(this.tableName)
   }
 }
--- backend/ms-business/database/migrations/1715378542681_plans.ts
+++ backend/ms-business/database/migrations/1715378542681_plans.ts
@@ -1,14 +1,16 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'admins'
+  protected tableName = 'plans'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('user_id')
-      table.string('direction')
-      table.integer('headquarter_id').unsigned().references('headquarter_id')
+      table.string('name')
+      table.string('description')
+      table.float('price')
+      
+
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */

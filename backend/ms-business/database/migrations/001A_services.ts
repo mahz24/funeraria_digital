47428644ea7c9c3077e0d4ce7bd2e43419437765
eldari18 <juandario050201@gmail.com>
2024-05-12 00:05:05 -0500

    1part Migration

--- backend/ms-business/database/migrations/001A_services.ts
+++ backend/ms-business/database/migrations/001A_services.ts
@@ -1,23 +1,21 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'departments'
+  protected tableName = 'services'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name')
-      table.string('location')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('ubicacion_cuerpo')
+      table.boolean('recoger_cuerpo')
+      table.string('estado')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
+
